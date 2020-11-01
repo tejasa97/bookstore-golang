2b2569f3389553a9154762d24dfbde7d17162539
Tejas Arlimatti <tejasarlimatti@gmail.com>
2020-11-01 17:18:17 +0530

    Added the Uber Zap logger

--- users/app/application.go
+++ users/app/application.go
@@ -1,6 +1,9 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tejasa97/bookstore_users-api/logger"
+)
 
 var (
 	router = gin.Default()
@@ -8,5 +11,7 @@ var (
 
 func StartApplication() {
 	MapUrls()
+
+	logger.Info("starting the app...")
 	router.Run(":8000")
 }
--- users/go.mod
+++ users/go.mod
@@ -5,6 +5,8 @@ go 1.15
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/mattn/go-sqlite3 v1.14.4
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
 	gorm.io/driver/sqlite v1.1.3
 	gorm.io/gorm v1.20.1
